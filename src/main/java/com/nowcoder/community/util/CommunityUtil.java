90c771860aafc9e52231fa42a39ae7cf35c04f16
Lijiawei <743736034@qq.com>
2021-11-21 17:17:31 +0800

    第三次提交

---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    //生成随机字符串
+    public static String generateUUID(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+    //MD5加密
+    //hello->abc123def46
+    //hello+3e4a8(随机字符串)-->abc123def46abc
+    public static String md5(String key){
+        if(StringUtils.isBlank(key)){
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+}
