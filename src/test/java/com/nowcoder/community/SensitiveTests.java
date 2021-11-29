be34c9b65655183c3305492339fca33ffe6b43a1
Lijiawei <743736034@qq.com>
2021-11-29 13:25:09 +0800

    3.2发布帖子

--- src/test/java/com/nowcoder/community/SensitiveTests.java
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -2,6 +2,7 @@ package com.nowcoder.community;
 
 import com.nowcoder.community.util.SensitiveFilter;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
