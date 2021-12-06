fae54f5a5383d940b70b3751929013d2586b5d28
Lijiawei <743736034@qq.com>
2021-12-06 20:50:10 +0800

    4.5关注、取消关注

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -1,24 +1,45 @@
 package com.nowcoder.community.util;
 
 public interface CommunityConstant {
-     //激活成功
-    int ACTIVATION_SUCCESS=0;
 
-    //重复激活
-    int ACTIVATION_REPEAT=1;
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
 
-    //激活失败
-    int ACTIVATION_FAILURE=2;
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
 
-    //默认状态的登录凭证超时时间 12hour
-    int DEFAULT_EXPIRED_SECONDS=3600*12;
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
 
-    //记住状态的登陆超时时间 100天
-    int REMEMBER_EXPIRED_SECONDS=3600*24*100;
+    /**
+     * 默认状态的登录凭证的超时时间
+     */
+    int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
 
-    //实体类型：帖子
-    int ENTITY_TYPE_POST=1;
+    /**
+     * 记住状态的登录凭证超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
-    //实体类型：评论
-    int ENTITY_TYPE_COMMENT=2;
+    /**
+     * 实体类型: 帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型: 评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
+
+    /**
+     * 实体类型: 用户
+     */
+    int ENTITY_TYPE_USER = 3;
+
 }
